aaa5b35e335b07dce334c55a8fef53206eee04c0
jinchuantang <jinchuant@gmail.com>
2023-04-06 09:49:46 -0700

    modify CI

---
+++ powershell/CreateEnv.ps1
@@ -0,0 +1,10 @@
+$pathPrefix = $args[0]
+$pathSuffix = "\WorkBC Career Transition Tool WEB/careerTransitionTool\.env.production"
+$filepath = Join-Path $pathPrefix $pathSuffix
+Write-Output $filepath
+$publicUrl = "GATSBY_PUBLIC_URL=./"
+$apiUrl = "GATSBY_API_URL=./api"
+$carouselNoc = "CAROUSEL_IMAGES=2122,2175"
+Add-Content $filepath $publicUrl 
+Add-Content $filepath $apiUrl
+Add-Content $filepath $carouselNoc
