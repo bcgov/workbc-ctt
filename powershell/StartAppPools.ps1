5c1a36d9209714f709c275995d0f601c3cd0b886
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2023-04-06 10:30:42 -0700

    Update StartAppPools.ps1

--- powershell/StartAppPools.ps1
+++ powershell/StartAppPools.ps1
@@ -1,5 +1,6 @@
 $appCmd = "C:\Windows\system32\inetsrv\appcmd.exe"
 $appcmd_args = "start", "appPool", $args[0]
 & $appCmd $appcmd_args
+Start-Sleep -s 120
 $appcmd_args = "start", "appPool", $args[1]
 & $appCmd $appcmd_args
