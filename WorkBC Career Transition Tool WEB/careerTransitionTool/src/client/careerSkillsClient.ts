687d50932037ecb76bc85969d324bbb76c48eb74
infojunkie <karim.ratib@gmail.com>
2023-12-15 13:50:18 -0800

    [WBCAMS-107] Fix missing filters by memoizing the API values

--- WorkBC Career Transition Tool WEB/careerTransitionTool/src/client/careerSkillsClient.ts
+++ WorkBC Career Transition Tool WEB/careerTransitionTool/src/client/careerSkillsClient.ts
@@ -6,14 +6,15 @@ import { ep } from './index'
 import { AutocompleteParams, GetAutocompleteResponse, UseGetAutocompleteResponse, GetTransferrableSkillParams, GetTransferrableSkillsResponse, GetReferenceTypeResponse, UseGetReferenceTypeResponse, ReferenceType, UseGetTransferrableSkillsResponse, GetSystemConfigurationParams, GetSystemConfigurationResponse, UseGetSystemConfigurationResponse } from "./careerSkillTypes"
 import { getAutoCompleteMinimumLength } from "../helpers/config"
 
-// Autocomplete 
+// Autocomplete
 export async function getAutocompleteCareerSkill(params: AutocompleteParams): Promise<GetAutocompleteResponse> {
     try {
       if (!params.partialNOCorTitle) return Promise.resolve([])
       if (params.partialNOCorTitle.length < getAutoCompleteMinimumLength(params.partialNOCorTitle)) return Promise.resolve([])
       const { partialNOCorTitle } = params
       const response: AxiosResponse<GetAutocompleteResponse> = await axios.get(ep.autoComplete.occupations, { params: {partialNOCorTitle: partialNOCorTitle}})
-      return Array.isArray(response.data) ? response.data : [] // temp solution to the api bug
+      return Array.isArray(response.data) ? response.data : [
+      ] // temp solution to the api bug
     } catch (error) {
       return []
     }
@@ -25,13 +26,90 @@ export function useGetAutocompleteCareerSkill(params: AutocompleteParams): UseGe
     return { skills, isValidating, isSettled: !!skills || !!error }
 }
 
-// Select 
+// Preloading and memoizing the lookups.
+let memoReferenceType = {
+  [ep.referenceType.salaryRanges]: [
+    {
+        "id": 1,
+        "value": "$11,000 - 33,000"
+    },
+    {
+        "id": 2,
+        "value": "$33,001 - 44,000"
+    },
+    {
+        "id": 3,
+        "value": "$44,001 - 53,000"
+    },
+    {
+        "id": 4,
+        "value": "$53,001 - 66,000"
+    },
+    {
+        "id": 5,
+        "value": "$66,001 - 95,000"
+    },
+    {
+        "id": 6,
+        "value": "$95,001 - 125,000"
+    },
+    {
+        "id": 7,
+        "value": "> $125,000"
+    }
+  ],
+  [ep.referenceType.workExperiences]: [
+    {
+        "id": 1,
+        "value": "None"
+    },
+    {
+        "id": 2,
+        "value": "1 year or less"
+    },
+    {
+        "id": 3,
+        "value": "> 1 year to 4 years"
+    },
+    {
+        "id": 4,
+        "value": "> 4 years to 10 years"
+    }
+  ],
+  [ep.referenceType.educationLevels]: [
+    {
+        "id": 1,
+        "value": "Less than High School"
+    },
+    {
+        "id": 2,
+        "value": "High School"
+    },
+    {
+        "id": 3,
+        "value": "Apprenticeship Certificate"
+    },
+    {
+        "id": 4,
+        "value": "Diploma/Certificate Excluding Apprenticeship"
+    },
+    {
+        "id": 5,
+        "value": "Degree"
+    }
+  ],
+  [ep.referenceType.similarities]: [{"id":1,"value":"All"},{"id":2,"value":"Medium"},{"id":3,"value":"High"}],
+}
+
+// Select
 export async function getReferenceType(url: string): Promise<GetReferenceTypeResponse> {
     try {
       if (!url) return Promise.resolve([])
 
       const response: AxiosResponse<GetReferenceTypeResponse> = await axios.get(url)
-      return Array.isArray(response.data) ? response.data : []
+      const result = Array.isArray(response.data) ? response.data : memoReferenceType[url];
+      memoReferenceType[url] = result;
+      return result
     } catch (error) {
       return []
     }
@@ -70,12 +148,13 @@ export async function getTransferrableSkills(params: GetTransferrableSkillParams
     try {
       const { code } = params
       const response: AxiosResponse<GetTransferrableSkillsResponse> = await axios.get(ep.occupations.occupations, { params: { NOC: code } })
-      return response.data
+      return Array.isArray(response.data) ? response.data : [
+      ]
     } catch (error) {
       return error.response.data
     }
   }
-  
+
 export function useGetTransferrableSkills(params: GetTransferrableSkillParams): UseGetTransferrableSkillsResponse {
     //const key = new URLSearchParams(params as any).toString()
     const { code } = params
