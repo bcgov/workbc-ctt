e3b07af6a65c63bcc0ef29010e9453f7d0715408
Sunandita Sahu <sunanditasahu@gmail.com>
2024-06-21 14:42:23 -0700

    Updated the nullable fields in Occupation class.

--- WorkBC Career Transition Tool API/Models/Occupation.cs
+++ WorkBC Career Transition Tool API/Models/Occupation.cs
@@ -9,12 +9,12 @@ namespace TransferrableSkillsToolAPI.Models
         public virtual List<CommonJobTitle> CommonJobTitles { get; set; }
         public string NOC { get; set; }
         public string Title { get; set; }
-        public int EducationId { get; set; }
+        public int? EducationId { get; set; }
         public EducationLevel Education { get; set; }
-        public int WorkExperienceId { get; set; }
+        public int? WorkExperienceId { get; set; }
         public WorkExperience WorkExperience { get; set; }
         public string Income { get; set; }
-        public int SalaryRangeId { get; set; }
+        public int? SalaryRangeId { get; set; }
         public Salary SalaryRange { get; set; }
     }
 }
