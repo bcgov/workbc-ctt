4df1531c9aaaa85afe72f57f1a5f72341a7755a6
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-08-13 16:25:25 -0700

    Update UpdateFrenchTitle.sql

--- SSOT/UpdateFrenchTitle.sql
+++ SSOT/UpdateFrenchTitle.sql
@@ -1,14 +1,13 @@
+--Alter column datatype to nvarchar for Title so that french characters can be saved.
 ALTER TABLE Occupations ALTER COLUMN Title nvarchar(max) not null
 
 DBCC CLEANTABLE (WorkBC_CareerTransitionTool, 'Occupations', 100000)
 
 ALTER TABLE Occupations REBUILD
-
-
 ------------------------------------------------------------------------------
---Update Occupations table records from temp table
+--Update Occupations table records for existing incorrect records.
 Update Occupations
-Set Title = (N'Maîtres d''hôtel and hosts / hostesses')
+Set Title = (N'MaÃ®tres d''hÃ´tel and hosts / hostesses')
 where NOC=64300;
 
 --Check results
